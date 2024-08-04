a5f817384926102faf3b22acce4e41d002a48429
Melissa Santos <melissasantos2202@gmail.com>
2024-08-04 14:00:44 -0600

    font-typography

--- pantry/app/page.js
+++ pantry/app/page.js
@@ -1,15 +1,9 @@
 
-import { Box } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 
 
 
-const items= [
-  'tomato',
-  'potato', 
-  'onion',
-  'garlic',
-  'ginger',
-  'carrot'
+const items= ['tomato','potato', 'onion','garlic', 'ginger','carrot', 'apple','oranges'
 ]
 export default function Home() {
   return( <Box
@@ -18,6 +12,25 @@ export default function Home() {
     display = {'flex'}
     justifyContent = {'center'}
     alignItems={'center'}
-  > Hello World</Box>
+  > <Stack width = '800px' height = '300px' spacing = {2} overflow = {'scroll'}> 
+    {items.map((i) => (
+      <Box 
+      key= {i}
+      width = "100%"
+      height = "300px"
+      display = {'flex'}
+      justifyContent = {'center'}
+      bgcolor = {'#8DA9C4'}
+    >
+      <Typography variant = {'h4'}
+      color = {'#333'}
+      textAlign = {'center'}
+      fontWeight = {'bold'}
+      >{i}</Typography>
+      
+    </Box>
+    ))}
+  
+  </Stack> </Box>
   )
 }
